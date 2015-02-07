4be4357e7e8bf4de0973ccf82f557ea71fc66adc
Simon Engelbrecht Sørensen <s.engelbrecht@gmail.com>
2015-02-07 15:16:49 +0100

    charge station

---
+++ Assets/Scripts/Power.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using Rewired;
+
+public class Power : MonoBehaviour {
+	public float power = 100;
+	bool inside;
+	Player player;
+	private bool pressA;
+	private bool used;
+
+	// Use this for initialization
+	void Start () {
+		player = ReInput.players.GetPlayer(0);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		power -= Time.deltaTime;
+		pressA = player.GetButtonDown("A");
+
+		if(inside && pressA && !used)
+		{
+			used = true;
+			power = 100;
+		}
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		if(col.CompareTag("powercharge"))
+		{
+			inside =true;
+		}
+	}
+	void OnTriggerExit(Collider col)
+	{
+		if(col.CompareTag("powercharge"))
+		{
+			inside =false;
+		}
+	}
+}
