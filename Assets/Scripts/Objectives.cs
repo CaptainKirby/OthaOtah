3175fac874a3ea9abe1fb083643437e2543caadb
Simon Engelbrecht Sørensen <s.engelbrecht@gmail.com>
2015-02-08 09:17:28 +0100

    stuffs

--- Assets/Scripts/Objectives.cs
+++ Assets/Scripts/Objectives.cs
@@ -17,21 +17,33 @@ public class Objectives : MonoBehaviour {
 	public int curClient;
 	public GameObject curClientObj;
 	public bool end;
+	public GUIText guiText1;
+	public GUIText guiText2;
+	private GameObject dirLight;
+
+	private BottleManager bMngr;
+
+	public GameObject drinksSpeech;
+	public GameObject needPowerSpeech;
 	void Start () 
 	{
+		bMngr = GetComponent<BottleManager>();
+		dirLight = GameObject.Find("Directional light");
 		barLocation = GameObject.FindGameObjectWithTag("pickup");
 		getDrink = true;
 		foreach(GameObject g in GameObject.FindGameObjectsWithTag("client"))
 		{
 			clients.Add(g);
 		}
+
+		StartCoroutine(Speech(drinksSpeech));
 	}
 	
 	void Update () 
 	{
 		if(end)
 		{
-
+			StartCoroutine(End());
 		}
 		if(getDrink && !spawnDrinkArrow && !end)
 		{
@@ -65,13 +77,13 @@ public class Objectives : MonoBehaviour {
 			spawnDeliverArrow = true;
 			if(deliverArrow == null)
 			{
-				deliverArrow = Instantiate(arrow, clients[curClient].transform.position, Quaternion.identity) as GameObject;
+				deliverArrow = Instantiate(arrow, new Vector3(clients[curClient].transform.position.x,clients[curClient].transform.position.y+4,clients[curClient].transform.position.z) , Quaternion.identity) as GameObject;
 			}
 			if(deliverArrow.activeSelf == false)
 			{
 
 				deliverArrow.SetActive(true);
-				deliverArrow.transform.position = clients[curClient].transform.position;
+				deliverArrow.transform.position = new Vector3(clients[curClient].transform.position.x,clients[curClient].transform.position.y+4,clients[curClient].transform.position.z) ;
 			}
 			curClientObj = clients[curClient].gameObject;
 //			curClient ++;
@@ -81,6 +93,28 @@ public class Objectives : MonoBehaviour {
 //		{
 //			spawnDrinkArrow = false;
 //		}
+
+	}
+	IEnumerator Speech(GameObject obj)
+	{
+		yield return new WaitForSeconds(1);
+		GameObject sB = Instantiate(obj, new Vector3(this.transform.position.x, this.transform.position.y + 3.5f, this.transform.position.z), Quaternion.identity) as GameObject;
+		yield return new WaitForSeconds(4);
+		sB.SetActive(false);
+
+	}
+	IEnumerator End()
+	{
+		dirLight.GetComponent<Light>().color = Color.black;
+		RenderSettings.ambientLight = Color.black;
+		yield return new WaitForSeconds(2);
+		GUIText g1 = Instantiate(guiText1, guiText1.transform.position, guiText1.transform.rotation) as GUIText;
+		GUIText g2 = Instantiate(guiText2, guiText2.transform.position, guiText2.transform.rotation) as GUIText;
+		g2.text = bMngr.successDeliveries.ToString() + " out of 9";
+
+		yield return new WaitForSeconds(2);
+		Application.LoadLevel(Application.loadedLevel);
+
 
 	}
 
